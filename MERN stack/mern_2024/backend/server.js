ef54e69c315da3124033b4eaa4923711f4d5e42d
tamutamu <tamu.0.0.tamu@gmail.com>
2024-06-16 01:38:43 +0900

    Renderデプロイ

--- MERN stack/mern_2024/backend/server.js
+++ MERN stack/mern_2024/backend/server.js
@@ -15,10 +15,10 @@ app.use("/api/users", usersRoutes);
 
 // Connecting to MongoDB using Mongoose
 mongoose
-  .connect("mongodb://localhost:27017", { dbName: "demo_db" })
+  .connect(process.env.MONGO_URI, { dbName: "demo_db" })
   .then(() => {
     console.log("connected to DB successfully");
-    
+
     // Listening to requests if DB connection is successful
     app.listen(4000, "localhost", () => console.log("Listening to port 4000"));
   })
