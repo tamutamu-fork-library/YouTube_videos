25e4ce38f1d9ffc32b147e58ad403973badde763
tamutamu <tamu.0.0.tamu@gmail.com>
2024-06-16 03:03:37 +0900

    wip..

--- MERN stack/mern_2024/backend/server.js
+++ MERN stack/mern_2024/backend/server.js
@@ -6,6 +6,23 @@ import { usersRoutes } from "./routes/usersRoutes.js";
 // Initializing Express app
 const app = express();
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  )
+
+  // intercept OPTIONS method
+  if ('OPTIONS' === req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
+app.use(allowCrossDomain)
+
 // Middleware to receive JSON
 app.use(express.json());
 
